56e28259267199457086f05de89139afc66170a4
juneyoung-jo@github.com <juneyoung-jo@github.com>
2023-02-04 17:39:33 +0900

    feat : add adapter pattern - 1

---
+++ src/patterns/structural/Adapter.kt
@@ -0,0 +1,30 @@
+package patterns.structural
+
+fun main() {
+    val adaptee = Adaptee()
+    val target = Adapter(adaptee)
+    target.request()
+}
+
+class Adapter(
+    private val adaptee: Adaptee
+) : Target {
+
+    override fun request() {
+        adaptee.specificRequest()
+    }
+
+}
+
+// 클라이언트가 사용하려는 인터페이스
+interface Target {
+    fun request()
+}
+
+// 이미 개발되어 수정이 어려운 기능
+class Adaptee {
+    fun specificRequest() {
+        println("Specific request.")
+    }
+}
+
